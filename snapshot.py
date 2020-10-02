cccd547fc4c6a9f835d5ffae0222cf7b1054e6eb
Dan C Williams <dan.c.williams@gmail.com>
2020-10-02 16:20:07 -0500

    cleaning up

--- snapshot.py
+++ snapshot.py
@@ -1,9 +1,14 @@
 import os
 import yfinance as yf
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+end = str(date.today())
+start = str(date.today() + relativedelta(months=-6))
 
 with open('symbols.csv') as f:
     lines = f.read().splitlines()
     for symbol in lines:
         print(symbol)
-        data = yf.download(symbol, start="2020-01-01", end="2020-10-01")
+        data = yf.download(symbol, start=start, end=end)
         data.to_csv("datasets/{}.csv".format(symbol))
